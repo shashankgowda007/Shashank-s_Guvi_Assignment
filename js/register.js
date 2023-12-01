fbab3f18a86b18e9ac9e2dfc7f57c939950189ee
shashankgowda-007 <144510118+shashankgowda-007@users.noreply.github.com>
2023-12-01 16:42:04 +0530

    update

--- js/register.js
+++ js/register.js
@@ -1,59 +1,19 @@
-function register() {
-    // Validate form fields
-    var formData = {
-        firstName: document.getElementById('firstName').value,
-        lastName: document.getElementById('lastName').value,
-        address: document.getElementById('address').value,
-        postCode: document.getElementById('postCode').value,
-        country: document.getElementById('country').value,
-        email: document.getElementById('email').value,
-        phoneNumber: document.getElementById('phoneNumber').value,
-        password: document.getElementById('password').value,
-        confirmPassword: document.getElementById('confirmPassword').value
-    };
+$(document).ready(function () {
+    $("#registerButton").on("click", function () {
+        register();
+    });
 
-    // Perform client-side validation
-    if (!validateForm(formData)) {
-        return;
-    }
-
-    // Send data to the server using AJAX
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/Shashank-s_Guvi_Assignment/php/register.php', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
-                // Display the result on the page
-                document.getElementById('registrationResult').textContent = xhr.responseText;
-            } else {
-                console.error('AJAX Error:', xhr.status, '-', xhr.statusText);
+    function register() {
+        $.ajax({
+            type: "POST",
+            url: "../php/register.php",
+            data: $("#registerForm").serialize(),
+            success: function (response) {
+                $("#registrationResult").html(response);
+            },
+            error: function (error) {
+                console.log(error);
             }
-        }
-    };
-
-    // Convert form data to URL-encoded format
-    var urlEncodedData = Object.keys(formData)
-        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(formData[key]))
-        .join('&');
-
-    xhr.send(urlEncodedData);
-}
-
-// Function to perform client-side validation
-function validateForm(formData) {
-    for (var key in formData) {
-        if (!formData[key]) {
-            alert('All fields are required.');
-            return false;
-        }
+        });
     }
-
-    if (formData.password !== formData.confirmPassword) {
-        alert('Password and Confirm Password must match.');
-        return false;
-    }
-
-    return true;
-}
+});
--- php/register.php
+++ php/register.php
@@ -1,51 +1,50 @@
 <?php
 
-// $servername = "your_mysql_server";
-// $username = "your_mysql_username";
-// $password = "your_mysql_password";
-// $dbname = "your_mysql_database";
+// Database connection details
+$host = "sql12.freesqldatabase.com";
+$database = "sql12666680";
+$username = "sql12666680";
+$password = "HQgTRrvBfs";
+$port = 3306;
 
-// Create connection
-$conn = new mysqli('localhost', 'root', '', 'guvi_assignment_shashank');
+$conn = new mysqli($host, $username, $password, $database, $port);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve user input
-    $firstName = $_POST["firstName"];
-    $lastName = $_POST["lastName"];
-    $address = $_POST["address"];
-    $postCode = $_POST["postCode"];
-    $country = $_POST["country"];
-    $email = $_POST["email"];
-    $phoneNumber = $_POST["phoneNumber"];
-    $password = $_POST["password"];
-    $confirmPassword = $_POST["confirmPassword"];
+    // Retrieve and sanitize form data
+    $firstName = mysqli_real_escape_string($conn, $_POST["firstName"]);
+    $lastName = mysqli_real_escape_string($conn, $_POST["lastName"]);
+    $gender = mysqli_real_escape_string($conn, $_POST["gender"]);
+    $DOB = mysqli_real_escape_string($conn, $_POST["DOB"]);
+    $address = mysqli_real_escape_string($conn, $_POST["address"]);
+    $postCode = mysqli_real_escape_string($conn, $_POST["postCode"]);
+    $country = mysqli_real_escape_string($conn, $_POST["country"]);
+    $email = mysqli_real_escape_string($conn, $_POST["email"]);
+    $phoneNumber = mysqli_real_escape_string($conn, $_POST["phoneNumber"]);
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT); // Hash the password
+    $confirmPassword = mysqli_real_escape_string($conn, $_POST["confirmPassword"]);
 
-    // Check if email or phone number already exists
-    $checkQuery = "SELECT * FROM user WHERE email = ? OR phoneNumber = ?";
+    // Check if email already exists
+    $checkQuery = "SELECT * FROM users WHERE email = ?";
     $checkStmt = $conn->prepare($checkQuery);
-    $checkStmt->bind_param("ss", $email, $phoneNumber);
+    $checkStmt->bind_param("s", $email);
     $checkStmt->execute();
     $checkResult = $checkStmt->get_result();
 
     if ($checkResult->num_rows > 0) {
-        // User already exists
-        echo "User with the same email or phone number already exists!";
+        echo "User with the same email already exists!";
     } else {
         // Insert new user data
-        $insertQuery = "INSERT INTO user (firstName, lastName, address, postCode, country, email, phoneNumber, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        $insertQuery = "INSERT INTO users (firstName, lastName, gender, DOB, address, postCode, country, email, phoneNumber, password, confirmPassword) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         $insertStmt = $conn->prepare($insertQuery);
-        $insertStmt->bind_param("ssssssss", $firstName, $lastName, $address, $postCode, $country, $email, $phoneNumber, $password);
+        $insertStmt->bind_param("sssssssssss", $firstName, $lastName, $gender, $DOB, $address, $postCode, $country, $email, $phoneNumber, $password, $confirmPassword);
 
         if ($insertStmt->execute()) {
-            // Registration successful
             echo "Registration successful!";
         } else {
-            // Error during registration
             echo "Error during registration: " . $insertStmt->error;
         }
 
